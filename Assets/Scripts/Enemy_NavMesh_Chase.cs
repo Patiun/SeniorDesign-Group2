7413d153c8572bea721f44b6f3bb49593f8fd795
Roger White <roger427@comcast.net>
2018-03-12 17:06:41 -0400

    Added tutorial level with non-vr character controller and nav mesh enemy to chase player

---
+++ Assets/Scripts/Enemy_NavMesh_Chase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;                                   //Needed for navmesh
+
+public class Enemy_NavMesh_Chase : MonoBehaviour {
+
+    public Transform chaseGoal;                         //What enemy runs to
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = chaseGoal.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.destination = chaseGoal.position;
+	}
+}
