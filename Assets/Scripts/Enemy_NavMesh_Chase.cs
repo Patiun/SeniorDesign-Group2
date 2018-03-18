f6b3f53a8aae516322431bdef334d6b45d54c2ac
Roger White <roger427@comcast.net>
2018-03-18 19:16:36 -0400

    updated tutorial level to VR friendly, added enemy, gun, and enemy health

--- Assets/Scripts/Enemy_NavMesh_Chase.cs
+++ Assets/Scripts/Enemy_NavMesh_Chase.cs
@@ -8,6 +8,9 @@ public class Enemy_NavMesh_Chase : MonoBehaviour {
     public Transform chaseGoal;                         //What enemy runs to
     private NavMeshAgent agent;
 
+    public float health;
+    public float dmgPerBullet;
+
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
@@ -18,4 +21,17 @@ public class Enemy_NavMesh_Chase : MonoBehaviour {
 	void Update () {
         agent.destination = chaseGoal.position;
 	}
+
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "AgentBullet"){
+            health = health - dmgPerBullet;
+            if(health <= 0){
+                Death();
+            }
+        }
+    }
+
+    void Death(){
+        Destroy(gameObject);
+    }
 }
