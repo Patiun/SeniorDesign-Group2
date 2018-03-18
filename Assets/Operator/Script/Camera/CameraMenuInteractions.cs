7997e33ddacbb12199f2bfbae24b0264f6c60000
Alexander <ac3433@drexel.edu>
2018-03-18 13:33:27 -0400

    Changes  - Made menu system to be more abstract to execute the functionality of the object than only camera.
    Fixes
     - Smoothing menu dropdown and selection area

---
+++ Assets/Operator/Script/Camera/CameraMenuInteractions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMenuInteractions : AbstractIconMenuButton
+{
+
+    public override void Disable()
+    {
+        Debug.LogWarning("Camera Menu Interaction script not implemented Disable");
+    }
+
+    public override void Enable()
+    {
+        Debug.LogWarning("Camera Menu Interaction script not implemented Enable");
+    }
+
+    public override void Execute()
+    {
+        Camera cam = InteractiveObject.GetComponent<Camera>();
+        CameraController.switchCamera(cam);
+    }
+}
