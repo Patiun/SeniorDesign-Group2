35791c24f8846090baa2c77f1d8ecee0e03c5133
Remlik <Remlikmg@comcast.net>
2018-04-04 18:28:34 -0400

    Crated DebugController.cs for cheat keys
    The tilda key currently toggles Debug mode and we can add debug keys into the update function.

---
+++ Assets/Scripts/DebugController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugController : MonoBehaviour {
+
+	public bool isDebugging = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("`")) {
+			isDebugging = !isDebugging;
+			if (isDebugging) {
+				Debug.Log ("Debug Mode is Enabled");
+			} else {
+				Debug.Log ("Debug Mode is Disabled");
+			}
+		}
+
+		if (isDebugging) {
+
+
+
+		}
+	}
+}
