74b216bccf07267fbbcf161dda4016539b9d3c8b
Roger White <roger427@comcast.net>
2018-04-30 21:13:46 -0400

    We went to staples

---
+++ Assets/SoundEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour {
+
+    public Transform noiseSphere;
+    public float TimeUnitlSilent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnCollisionEnter(Collision collision)
+	{
+        Instantiate(noiseSphere, gameObject.transform.position, Quaternion.identity);
+        float scale = collision.relativeVelocity.magnitude;
+        noiseSphere.transform.localScale = new Vector3(scale, scale, scale);
+        Destroy(noiseSphere, TimeUnitlSilent);
+	}
+}
