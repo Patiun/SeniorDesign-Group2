38998a4bab48b75dbad33e9f83b87ded847a4519
Remlik <Remlikmg@comcast.net>
2018-05-07 20:50:30 -0400

    Added PlayerHealth to scene under the WorldController

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int maxHP;
+	public int curHP;
+	// Use this for initialization
+	void Start () {
+		curHP = maxHP;
+	}
+	
+	public void DoDamage(int dmg) {
+		curHP -= dmg;
+		if (curHP) {
+			GetComponent<GameState> ().LoseLevel ();
+			curHP = 0;
+		}
+	}
+
+	public void Heal(int amnt) {
+		curHP += amnt;
+		if (curHP > maxHP) {
+			curHP = maxHP;
+		}
+	}
+}
