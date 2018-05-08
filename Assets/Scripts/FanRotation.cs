597f9c60ba6b9e911b7a686eb522c0a99578fd1d
Roger White <roger427@comcast.net>
2018-05-07 20:58:01 -0400

    Fan Rotation, Fan Instakill
    Added Script for fan rotation, stopping fan rotation, killing player on enter while on.

---
+++ Assets/Scripts/FanRotation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotation : MonoBehaviour {
+
+    public float fanSpeed;
+    public bool on;
+    public int timeInactive;
+    public PlayerHealth playerHealth;
+
+    private Quaternion originalRotation;
+    private int count;
+
+	// Use this for initialization
+	void Start () {
+        originalRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (on == true)
+        {
+            count = 0;
+            transform.Rotate(Vector3.forward * (fanSpeed * Time.deltaTime));
+        }
+        if(on == false){
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotation, fanSpeed * Time.deltaTime);
+            if (count >= timeInactive){
+                on = true;
+            }
+            else{
+                count++;
+            }
+        }
+	}
+
+	public void OnTriggerEnter(Collider collider)
+	{
+        if(collider.gameObject.tag == "Player"){
+            playerHealth.InstaKill();
+        }
+	}
+}
