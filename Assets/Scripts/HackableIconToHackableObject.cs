9041080b2f6793a0074b4e6e0fe1c22ab96b18f3
Remlik <Remlikmg@comcast.net>
2018-05-10 02:24:53 -0400

    Started messing with secuirty cameras in scene and in camera canvases with hacking notifications

---
+++ Assets/Scripts/HackableIconToHackableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HackableIconToHackableObject : MonoBehaviour {
+
+	public GameObject target;
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = camera.WorldToScreenPoint (target.transform.position);
+	}
+}
