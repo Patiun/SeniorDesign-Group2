5b90d3fc7622a6920ac24135324591a212a4782c
Remlik <Remlikmg@comcast.net>
2018-05-09 22:02:09 -0400

    Vent Covers!

---
+++ Assets/Scripts/ForceVentSnap.cs
@@ -0,0 +1,20 @@
+﻿namespace VRTK {
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class ForceVentSnap : MonoBehaviour {
+
+		public GameObject vent;
+
+		// Use this for initialization
+		void Start () {
+			GetComponent<VRTK_SnapDropZone> ().ForceSnap (vent);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
