9e60a5326296204740e9f0051092e6aa6592e7ca
Remlik <Remlikmg@comcast.net>
2018-05-12 21:37:17 -0400

    Automatic Doors work for Enemies and enemies don't get stuck in rooms randomly anymore

---
+++ Assets/Scripts/DoorAutomaticOpen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAutomaticOpen : MonoBehaviour {
+
+	public Door door;
+	public List<string> accepted;
+	private ObjectStrangeLocation locationLogic;
+
+	// Use this for initialization
+	void Start () {
+		locationLogic = GetComponent<ObjectStrangeLocation> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider col) {
+		if (accepted.Contains(col.gameObject.tag) && !col.isTrigger) {
+			door.Open();
+		}
+	}
+
+	public void OnTriggerStay(Collider col) {
+		if (accepted.Contains(col.gameObject.tag) && !col.isTrigger) {
+			door.Open();
+		}
+	}
+
+	public void OnTriggerExit(Collider col) {
+		if (accepted.Contains(col.gameObject.tag) && !col.isTrigger) {
+			Debug.Log ("Enemy Left");
+			door.Close();
+			//locationLogic.lastPosition = door.gameObject.transform.position;
+		}
+	}
+}
