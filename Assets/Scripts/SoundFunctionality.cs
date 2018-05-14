2675c1e996e68b18852a984ee4c8c15cfc7efdee
Roger White <roger427@comcast.net>
2018-05-14 13:24:27 -0400

    Only closest enemy investigates noise

--- Assets/Scripts/SoundFunctionality.cs
+++ Assets/Scripts/SoundFunctionality.cs
@@ -6,14 +6,40 @@ public class SoundFunctionality : MonoBehaviour {
 
 	public float timeLimit = .5f;
 	private float count = 0;
+	private List<GameObject> enemies;
+	private GameObject closestEnemy;
 	// Use this for initialization
 	void Start () {
-		
+		enemies = new List<GameObject>();
+		closestEnemy = null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (count >= timeLimit) {
+		if (count >= timeLimit)
+		{
+			float minDistance = -1;
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				if (i == 0)
+				{
+					minDistance = Vector3.Distance(transform.position, enemies[0].transform.position);
+					closestEnemy = enemies[0].gameObject;
+				}
+				else
+				{
+					float dist = Vector3.Distance(transform.position, enemies[i].transform.position);
+					if (dist < minDistance)
+					{
+						minDistance = dist;
+						closestEnemy = enemies[i].gameObject;
+					}
+				}
+			}
+			if (closestEnemy != null) {
+    			EnemyAI brain = closestEnemy.GetComponent<EnemyAI>();
+    			brain.MinorActivity(transform.position);
+		    }
 			Destroy (this.gameObject);
 		} else {
 			count += Time.deltaTime;
@@ -21,11 +47,8 @@ public class SoundFunctionality : MonoBehaviour {
 	}
 
 	public void OnTriggerEnter(Collider col) {
-		if (col.tag == "Enemy" && col.isTrigger == false) {
-			EnemyAI brain = col.gameObject.GetComponent<EnemyAI> ();
-			if (brain != null) {
-				brain.MinorActivity (transform.position);
-			}
+		if(col.tag == "Enemy" && col.isTrigger == false){
+			enemies.Add(col.gameObject);
 		}
 	}
 }
