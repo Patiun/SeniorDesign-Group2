015866de477b874d899352aaa379bc66e395d74a
Roger White <roger427@comcast.net>
2018-05-16 15:37:31 -0400

    All SO_Objects have destruction script

---
+++ Assets/Scripts/DestructableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableObject : MonoBehaviour {
+
+	public int ObjectHealth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void takeDamage(int dmg){
+		ObjectHealth = ObjectHealth - dmg;
+		if(ObjectHealth <= 0){
+			Destroy(gameObject);
+		}
+	}
+}
