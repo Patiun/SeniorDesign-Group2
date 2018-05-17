e0eb092ffef01a58aecaa12238d9eae6751ec102
ac3433 <ac3433@drexel.edu>
2018-05-17 18:26:03 -0400

    laser icon

---
+++ Assets/Scripts/LazerMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LazerMenu : AbstractHackMenu
+{
+    [SerializeField]
+    private LazerGroupBehavior _lazerObject;
+    [SerializeField]
+    private PuzzleDifficultiesLevel level;
+
+    public void Disable()
+    {
+        _lazerObject.groupOn = false;
+    }
+
+    public void Hack()
+    {
+        HackManager.Instance.InitializeHacking(this, level);
+    }
+
+    public override void NotifyHackStatus(bool status)
+    {
+        if(status)
+        {
+            GameObject hack = transform.Find("HackButton").gameObject;
+            if (hack != null)
+                hack.SetActive(false);
+
+            DisplayFullMenuWithoutHack();
+        }
+    }
+
+    //add fading to the disable button
+    public void DisplayFullMenuWithoutHack()
+    {
+        
+        GameObject disable = transform.Find("DisableButton").gameObject;
+
+        if (disable != null)
+            disable.SetActive(true);
+    }
+}
