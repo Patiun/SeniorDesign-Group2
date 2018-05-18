4bbbd1dd4d1a6b42a14b1641ed6c4dd448fb2a85
Roger White <roger427@comcast.net>
2018-05-18 17:01:06 -0400

    Sprites on all buttons and difficulty slection on main menu

---
+++ Assets/Scripts/MainMenu/DifficultySelect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultySelect : MonoBehaviour {
+    
+	public Material originalMaterial;
+	public GameObject buttonOne;
+	public GameObject buttonTwo;
+
+	private bool flash;
+
+	// Use this for initialization
+	void Start () {
+		flash = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void OnMouseDown(){
+		buttonOne.GetComponent<DifficultySelect>().flash = false;
+		buttonTwo.GetComponent<DifficultySelect>().flash = false;
+		gameObject.GetComponent<Renderer>().material = originalMaterial;
+        buttonOne.GetComponent<Renderer>().material = originalMaterial;
+		buttonTwo.GetComponent<Renderer>().material = originalMaterial;
+	}
+   
+}
