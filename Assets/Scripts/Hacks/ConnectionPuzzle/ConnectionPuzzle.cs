da500982d4bac45b1566c678d4b0f35454f6746a
Roger White <roger427@comcast.net>
2018-05-19 18:36:54 -0400

    New type of hacking puzzle in seperate scene

---
+++ Assets/Scripts/Hacks/ConnectionPuzzle/ConnectionPuzzle.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEditor.UI;
+
+public class ConnectionPuzzle : MonoBehaviour, IPointerDownHandler {
+
+	public GameObject[] PathNodes;
+	public GameObject[] PathLegs;
+	public GameObject PuzzleWatcher;
+	public float MoveSpeed;
+
+	private float Timer;
+	private int currentNode;
+	private Vector3 currentPositionHolder;
+	private bool isActive;
+    
+
+	private Vector3 OriginalBoxPosition;
+    
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.blue;
+		Timer = 0;
+		OriginalBoxPosition = gameObject.transform.position;
+		currentNode = 0;
+		currentPositionHolder = PathNodes[currentNode].transform.position;
+		isActive = false;
+
+		for (int i = 0; i < PathNodes.Length; i++){
+			PathLegs[i].gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.red;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (isActive == true)
+		{
+			Timer += Time.deltaTime * MoveSpeed;
+
+			if (gameObject.transform.position != currentPositionHolder)
+			{
+				gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, currentPositionHolder, Timer);
+			}
+			else
+			{
+				PathLegs[currentNode].gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.green;
+
+				if (currentNode < (PathNodes.Length - 1))
+				{
+					currentNode++;
+				}
+				Timer = 0;
+				currentPositionHolder = PathNodes[currentNode].transform.position;
+			}
+		}
+
+	}
+
+	public void OnPointerDown(PointerEventData eventData){
+		if (PuzzleWatcher.gameObject.GetComponent<PuzzleBheavior>().getHasCollided() == false)
+		{
+			isActive = true;
+		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D collision){
+		isActive = false;
+		gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.red;
+		PuzzleWatcher.gameObject.GetComponent<PuzzleBheavior>().setHasCollided(true);
+
+    }
+
+	public void Reset(){
+		Timer = 0;
+		PuzzleWatcher.gameObject.GetComponent<PuzzleBheavior>().setHasCollided(false);
+		gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.blue;
+		gameObject.transform.position = OriginalBoxPosition;
+        currentNode = 0;
+        currentPositionHolder = PathNodes[currentNode].transform.position;
+        isActive = false;
+
+		for (int i = 0; i < PathNodes.Length; i++)
+        {
+            PathLegs[i].gameObject.GetComponent<UnityEngine.UI.Image>().color = Color.red;
+        }
+	}
+
+
+}
---
+++ Assets/Scripts/Hacks/ConnectionPuzzle/PuzzleBheavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+
+public class PuzzleBheavior : MonoBehaviour {
+
+	public GameObject[] puzzles;
+	public float timer;
+	public GameObject timeLeftText;
+
+	private GameObject activePuzzle;
+	private float timeLeft;
+	private bool hasCollided;
+
+	// Use this for initialization
+	void Start () {
+		hasCollided = false;
+		timeLeft = timer;
+		timeLeftText.GetComponent<UnityEngine.UI.Text>().text = "Time Left: " + timeLeft;
+		activePuzzle = puzzles[Random.Range(0, puzzles.Length)];
+		activePuzzle.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		timeLeftText.GetComponent<UnityEngine.UI.Text>().text = "Time Left: " + timeLeft;
+	}
+    
+	public void Restart()
+	{
+		ConnectionPuzzle[] connectionPuzzles = activePuzzle.GetComponentsInChildren<ConnectionPuzzle>();
+		for (int i = 0; i < connectionPuzzles.Length; i++){
+			connectionPuzzles[i].Reset();
+		}
+
+	}
+
+	public void setHasCollided(bool col){
+		hasCollided = col;
+	}
+
+	public bool getHasCollided(){
+		return hasCollided;
+	}
+}
