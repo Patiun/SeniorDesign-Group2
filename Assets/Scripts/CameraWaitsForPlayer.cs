a1634341fe4cd36988075cf73540647e8c6c5dd7
Remlik <Remlikmg@comcast.net>
2018-05-20 19:30:20 -0400

    Security cameras wait for the player to be within usable range before they search for it, also added 3 cameras to the level_03

---
+++ Assets/Scripts/CameraWaitsForPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWaitsForPlayer : MonoBehaviour {
+
+	public List<GameObject> thingsToToggle;
+	public bool hasPlayer;
+
+	// Use this for initialization
+	void Start () {
+		ToggleOff ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player") {
+			hasPlayer = true;
+			ToggleOn ();
+		}
+	}
+
+	private void ToggleOn() {
+		foreach (GameObject go in thingsToToggle) {
+			go.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (col.tag == "Player") {
+			hasPlayer = false;
+			ToggleOff ();
+		}
+	}
+
+	private void ToggleOff() {
+		foreach (GameObject go in thingsToToggle) {
+			go.SetActive (false);
+		}
+	}
+}
