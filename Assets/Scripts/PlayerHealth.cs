c10a35f0c0eef90795b6a3ad30c153dd92d97571
James Swanick <swanijam@gmail.com>
2018-05-20 20:48:27 -0400

    Change Fog Color on Alert And Health

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,9 +7,13 @@ public class PlayerHealth : MonoBehaviour {
 	public int maxHP;
 	public int curHP;
 	public bool immortal = false;
+
+	public Color fogColorCalm, fogColorAlert, fogColorHurt;
+	public bool alert = false;
 	// Use this for initialization
 	void Start () {
 		curHP = maxHP;
+		UpdateFog ();
 	}
 	
 	public void DoDamage(int dmg) {
@@ -20,6 +24,7 @@ public class PlayerHealth : MonoBehaviour {
 				curHP = 0;
 			}
 		}
+		UpdateFog ();
 	}
 
 	public void Heal(int amnt) {
@@ -27,6 +32,7 @@ public class PlayerHealth : MonoBehaviour {
 		if (curHP > maxHP) {
 			curHP = maxHP;
 		}
+		UpdateFog ();
 	}
 
 	public void InstaKill() {
@@ -34,5 +40,16 @@ public class PlayerHealth : MonoBehaviour {
 			curHP = 0;
 			GetComponent<GameState> ().LoseLevel ();
 		}
+		UpdateFog ();
+	}
+
+	public void UpdateFog() {
+		alert = GetComponent<WorldState> ().isAlert;
+		Color useColor = alert ? fogColorAlert : fogColorCalm;
+		RenderSettings.fogColor = Color.Lerp (fogColorHurt, useColor, ((float)curHP)/((float)maxHP));
+	}
+
+	void Update() {
+		UpdateFog ();
 	}
 }
