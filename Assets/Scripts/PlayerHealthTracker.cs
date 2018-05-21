e3026662334cf7e42d6cd5a469e1903b26cd79df
Remlik <Remlikmg@comcast.net>
2018-05-21 00:03:52 -0400

    Added Healthbar for operator to see

---
+++ Assets/Scripts/PlayerHealthTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthTracker : MonoBehaviour {
+
+	public Image health;
+	public PlayerHealth playerHealth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		health.fillAmount = ((float)(playerHealth.curHP) / (float)(playerHealth.maxHP));
+	}
+}
