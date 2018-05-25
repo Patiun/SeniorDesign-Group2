6878a01331d0248e6327a99f8c53e273068bbcf9
Remlik <Remlikmg@comcast.net>
2018-05-25 01:49:57 -0400

    Room Prefabs are ready for iteration
    Dev_Level_003 under construction

---
+++ Assets/Scripts/ProceduralGeneration/ChooseRandomLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseRandomLevel : MonoBehaviour {
+
+	public int[] sceneIndexs;
+
+	public void OnClick() {
+		int rand = Random.Range (0, sceneIndexs.Length);
+		SceneManager.LoadScene (sceneIndexs[rand]);
+	}
+}
