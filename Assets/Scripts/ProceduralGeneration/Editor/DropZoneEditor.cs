32690015167b19dda6637c9402b13ae9769a1a89
Remlik <Remlikmg@comcast.net>
2018-05-25 20:47:02 -0400

    Drop Zone script is ready for use

---
+++ Assets/Scripts/ProceduralGeneration/Editor/DropZoneEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ObjectDropZone))]
+public class DropZoneEditor : Editor {
+
+	public override void OnInspectorGUI ()
+	{
+		base.OnInspectorGUI ();
+
+		ObjectDropZone zone = (ObjectDropZone)target;
+
+		zone.areaScaleX = GUILayout.HorizontalSlider (zone.areaScaleX, 0.1f, 10f);
+		zone.areaScaleY = GUILayout.HorizontalSlider (zone.areaScaleY, 0.1f, 10f);
+		zone.areaScaleZ = GUILayout.HorizontalSlider (zone.areaScaleZ, 0.1f, 10f);
+	}
+}
---
+++ Assets/Scripts/ProceduralGeneration/ObjectDropZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDropZone : MonoBehaviour {
+
+	public GameObject objectiveObject;
+	public float areaScaleX, areaScaleZ, areaScaleY;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			DropObject();
+		}
+	}
+		
+	public void DropObject() {
+		float randX = Random.Range (-areaScaleX, areaScaleX);
+		float randY = Random.Range (-areaScaleY, areaScaleY);
+		float randZ = Random.Range (-areaScaleZ, areaScaleZ);
+
+		Vector3 dropLocation = transform.position + new Vector3 (randX, randY, randZ);
+
+		GameObject objective = Instantiate (objectiveObject);
+		objective.transform.position = dropLocation;
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube (transform.position, new Vector3 (2*areaScaleX, 2*areaScaleY, 2*areaScaleZ));
+	}
+}
