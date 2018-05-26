a3f75dd67c1eff93c70880a937268a1b2a3f9890
Remlik <Remlikmg@comcast.net>
2018-05-25 20:19:53 -0400

    Added Windows to the editor to set up objects and make them function faster

---
+++ Assets/Scripts/ProceduralGeneration/Editor/NavObstacleWindow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NavObstacleWindow : EditorWindow {
+
+	private bool addNavMesh = true;
+	private bool addMeshCollider = true;
+
+	[MenuItem("Window/Operation Exodus/Setup Nav Obstacle")]
+	public static void ShowWindow() {
+		EditorWindow.GetWindow (typeof(NavObstacleWindow));
+	}
+
+	private void init() {
+		addNavMesh = true;
+		addMeshCollider = true;
+	}
+
+	void OnGUI() {
+		EditorGUILayout.Space ();
+		EditorGUILayout.LabelField ("Navigation Setup Options", EditorStyles.boldLabel);
+		addNavMesh = EditorGUILayout.Toggle ("Add a NavMeshSourceTag",addNavMesh);
+		addMeshCollider = EditorGUILayout.Toggle ("Add a Mesh Collider",addMeshCollider);
+		EditorGUILayout.Space ();
+		if (GUILayout.Button ("Update Selected",GUILayout.Height(40))) {
+			GameObject[] selected = Selection.gameObjects;
+			for (int i = 0; i < selected.Length; i++) {
+				if (addNavMesh && selected [i].GetComponent<NavMeshSourceTag> () == null) {
+					selected [i].AddComponent<NavMeshSourceTag> ();
+				}
+				if (addMeshCollider && selected [i].GetComponent<MeshCollider> () == null) {
+					selected [i].AddComponent<MeshCollider> ();
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ProceduralGeneration/Editor/ObjectInteractions.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ObjectInteractions : EditorWindow {
+
+	private bool setInteractable = true;
+
+	[MenuItem ("Window/Operation Exodus/Setup Object Interactions")]
+
+	public static void ShowWindow() {
+		EditorWindow.GetWindow (typeof(ObjectInteractions));
+	}
+
+	void OnGUI() {
+		//Change tag
+		setInteractable = EditorGUILayout.Toggle("Set Tag",setInteractable);
+		EditorGUILayout.Space();
+		if (GUILayout.Button("Update Selected",GUILayout.Height(40))){
+			GameObject[] selected = Selection.gameObjects;
+			for (int i = 0; i < selected.Length; i++) {
+				GameObject go = selected [i];
+				if (setInteractable) {
+					go.tag = "Interactible";
+				} else {
+					if (go.tag != "Interactible" && go.GetComponent<ObjectStrangeLocation> () != null) {
+						go.GetComponent<ObjectStrangeLocation> ().enabled = false;
+					}
+				}
+				//Add ObjectStrangeLocation
+				ObjectStrangeLocation objStrLoc = go.GetComponent<ObjectStrangeLocation> (); 
+				if (objStrLoc == null) {
+					go.AddComponent<ObjectStrangeLocation> ();
+				} else if (!objStrLoc.enabled && go.tag == "Interactible") {
+					objStrLoc.enabled = true;
+				}
+				//Add Destructable Object
+				DestructableObject destObj = go.GetComponent<DestructableObject> (); 
+				if (destObj == null) {
+					go.AddComponent<DestructableObject> ();
+				}
+				//Add SoundEffect
+				SoundEffect soundEffect = go.GetComponent<SoundEffect> (); 
+				if (soundEffect == null) {
+					go.AddComponent<SoundEffect> ();
+				}
+				MeshCollider mesh = go.GetComponent<MeshCollider> ();
+				if (mesh == null) {
+					MeshCollider newMesh = go.AddComponent<MeshCollider> ();
+					newMesh.convex = true;
+				} else {
+					mesh.convex = true;
+				}
+				Rigidbody rb = go.GetComponent<Rigidbody> ();
+				if (rb == null) {
+					go.AddComponent<Rigidbody> ();
+				}
+			}
+		}
+	}
+}
