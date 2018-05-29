610dcc79bcf469f5029a6e963e3283984fa7a67f
James Swanick <swanijam@gmail.com>
2018-05-29 15:14:38 -0400

    DragonCard with random face textures

---
+++ Assets/randomPortraitTexSelect.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomPortraitTexSelect : MonoBehaviour {
+	public Texture2D[] faces;
+	// Use this for initialization
+	void Start () {
+		GetComponent<MeshRenderer> ().material.mainTexture = faces [Random.Range (0, faces.Length - 1)];
+	}
+}
