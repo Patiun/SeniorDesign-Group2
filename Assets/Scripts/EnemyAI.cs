4ef436f55deb81e00328687c2df00c39719e032b
Remlik <Remlikmg@comcast.net>
2018-05-29 13:31:38 -0400

    Enemies aren't delayed as long at start

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -44,7 +44,7 @@ public class EnemyAI : MonoBehaviour {
 	}
 
 	IEnumerator WaitToPatrol() {
-		yield return new WaitForSeconds (5);
+		yield return new WaitForSeconds (3);
 		movement.ReturnToPatrol ();
 		isReady = true;
 	}
